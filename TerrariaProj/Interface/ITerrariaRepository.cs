b631df3d8966af716d21a1fa39203aa201382a5e
Illia <Illia@DESKTOP-TG4JUT1>
2023-07-08 15:24:32 +0200

    +add interface

---
+++ TerrariaProj/Interface/ITerrariaRepository.cs
@@ -0,0 +1,14 @@
+﻿using TerrariaProj.Models;
+
+namespace TerrariaProj.Interface
+{
+    public interface ITerrariaRepository
+    {
+        IEnumerable<Player> GetPlayers();
+        Player GetPlayerByName(string name);
+        void CreatePlayer(Player player);
+        void UpdatePlayer(Player player);
+        void DeletePlayer(string name);
+
+    }
+}
