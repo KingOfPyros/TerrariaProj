59db6a8fad6aa182bf2f462b632d9a58cf4286be
Illia <Illia@DESKTOP-TG4JUT1>
2023-07-08 15:40:48 +0200

    +add controller

---
+++ TerrariaProj/Controllers/TerrariaController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TerrariaProj.Interface;
+using TerrariaProj.Models;
+
+namespace TerrariaProj.Controllers
+{
+    [ApiController]
+    [Route("api/terraria")]
+    public class TerrariaController : ControllerBase
+    {
+        private readonly ITerrariaRepository _repository;
+
+        public TerrariaController(ITerrariaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("players")]
+        public IActionResult GetPlayers()
+        {
+            var players = _repository.GetPlayers();
+            return Ok(players);
+        }
+
+        [HttpGet("players/{name}")]
+        public IActionResult GetPlayerByName(string name)
+        {
+            var player = _repository.GetPlayerByName(name);
+            if (player == null)
+                return NotFound();
+
+            return Ok(player);
+        }
+
+        [HttpPost("players")]
+        public IActionResult CreatePlayer(Player player)
+        {
+            _repository.CreatePlayer(player);
+            return CreatedAtAction(nameof(GetPlayerByName), new { name = player.Name }, player);
+        }
+
+        [HttpPut("players/{name}")]
+        public IActionResult UpdatePlayer(string name, Player updatedPlayer)
+        {
+            var player = _repository.GetPlayerByName(name);
+            if (player == null)
+                return NotFound();
+
+            player.Name = updatedPlayer.Name;
+            player.Health = updatedPlayer.Health;
+            player.Armor = updatedPlayer.Armor;
+
+            _repository.UpdatePlayer(player);
+            return NoContent();
+        }
+
+        [HttpDelete("players/{name}")]
+        public IActionResult DeletePlayer(string name)
+        {
+            var player = _repository.GetPlayerByName(name);
+            if (player == null)
+                return NotFound();
+
+            _repository.DeletePlayer(name);
+            return NoContent();
+        }
+    }
+}
---
+++ TerrariaProj/TerrariaRepository.cs
@@ -0,0 +1,33 @@
+﻿using TerrariaProj.Interface;
+using TerrariaProj.Models;
+
+namespace TerrariaProj
+{
+    public class TerrariaRepository : ITerrariaRepository
+    {
+        public void CreatePlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePlayer(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player GetPlayerByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Player> GetPlayers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
